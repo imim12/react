85827028e90a8f7421f3538f65377a63cb3f10c9
imj <imijij25@gamil.com>
2023-08-15 10:46:40 +0900

    01-11 image Lazy Loading까지

---
+++ src/components/LazyImage.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+const LazyImage = ({url, alt}) => {
+    const [isLoading,setIsLoading] = useState(true);  //초기값 true  //5. 랜더링. 
+    const [opacity, setOpacity] = useState('opacity-0'); 
+
+    useEffect(()=>{  //1.   //랜더링 과정을 생각해봄. 5번이 먼저 일어나고 3번이 일어나는지 잘 모르겠음
+        isLoading ? setOpacity('opacity-0') : setOpacity('opacity-100');  //4.  setOpacity값 변경
+    },[isLoading])  //3. isLoading 값이 바뀔때마다 useEffect 실행
+
+  return (
+    <>
+        {isLoading && (   //6. 랜더링 되면서 isLoading 값이 false이기 때문에 여긴 자동으로 안 보임
+            <div className='absolute h-full  z-10 w-full flex items-center justify-center'>
+                ...loading
+            </div>
+        )}
+        <img 
+            src={url}
+            alt={alt}
+            width="100%"
+            height="auto"
+            loading="lazy"
+            onLoad={()=>setIsLoading(false)} //2. //onLoad= 이미지 로딩이 다 될때의 상태를 말함. setIsLoading(false)값을 줘서 ...loading이 안보이게함  
+            className={`object-contain h-full ${opacity}`} //6. 바뀐 값이 opacity에 들어감
+        />
+    </>
+  )
+}
+
+export default LazyImage
---
+++ src/components/PokeCard.jsx
@@ -0,0 +1,66 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import LazyImage from './LazyImage';
+
+const PokeCard = ({url, name}) => {
+ 
+    const [pokemon, setPokemon] = useState();
+
+    useEffect(() => {
+        fetchPokeDetailData()
+    }, [])
+    
+    async function fetchPokeDetailData(){
+        try {
+            const response = await axios.get(url)
+            console.log(response.data)
+            const pokemonData = formatPokemonData(response.data);
+            setPokemon(pokemonData)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+ 
+    function formatPokemonData(params){
+        const {id, types, name} = params;
+        const PokeData = {
+            id,
+            name,
+            type: types[0].type.name
+        }
+        return PokeData;
+    }
+    const bg = `bg-${pokemon?.type}`;   //?는 만약 pokemon이 데이터가 있으면 type을 가져오는것. 없어도 오류 안내고
+    const border = `border-${pokemon?.type}`;
+    const text = `text-${pokemon?.type}`;
+
+    const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+    
+  return (
+    <>
+    {pokemon &&  //pokemon && 는 if문처럼 사용하는 것으로 && 앞이 true일때(그냥 데이터도 true임) 그 뒤의 코드 실행. 또 이 안에서 pokemon 사용하려면 pokemon이 있어야함
+        <a 
+            href={`/pokemon/${name}`}
+            className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 justify-between items-center`}
+        >   
+            <div className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2  text-right rounded-t-lg`}>
+                #{pokemon.id.toString().padStart(3,'00')}
+            </div>
+            <div className={`w-full f-6 flex items-center justify-center`}>
+                <div className={`box-border relative flex w-full h-[5.5rem] basis justify-center items-center`}>
+                    <LazyImage
+                        url={img}
+                        alt={name}
+                    />
+                </div>
+            </div>
+            <div className={`${bg} text-center text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium  pt-1`}>        
+                {pokemon.name}
+            </div>
+        </a>
+    }
+    </>
+  )
+}
+
+export default PokeCard
